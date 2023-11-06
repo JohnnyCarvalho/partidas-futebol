bf670155286636e5072a9156b549cf48c9221361
JohnnyCarvalho <johnnycarvalhodev@gmail.com>
2023-11-06 11:00:32 -0300

    Criando projeto spring.

---
+++ cadastroFutebolAPI/src/main/java/br/com/meli/cadastrofutebolapi/CadastroFutebolApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.meli.cadastrofutebolapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroFutebolApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CadastroFutebolApiApplication.class, args);
+    }
+
+}
---
+++ cadastroFutebolAPI/src/test/java/br/com/meli/cadastrofutebolapi/CadastroFutebolApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meli.cadastrofutebolapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroFutebolApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
