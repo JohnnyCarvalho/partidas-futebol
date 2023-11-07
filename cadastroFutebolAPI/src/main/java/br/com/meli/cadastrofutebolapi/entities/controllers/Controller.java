d6d465d126b28dae4b2958e6ec91c2d9f7392e51
JohnnyCarvalho <johnnycarvalhodev@gmail.com>
2023-11-07 10:47:11 -0300

    Configurando conexão com banco mysql.

--- cadastroFutebolAPI/src/main/java/br/com/meli/cadastrofutebolapi/entities/controllers/Controller.java
+++ cadastroFutebolAPI/src/main/java/br/com/meli/cadastrofutebolapi/entities/controllers/Controller.java
@@ -1,4 +1,5 @@
 package br.com.meli.cadastrofutebolapi.entities.controllers;
 
+
 public class Controller {
 }
--- cadastroFutebolAPI/src/main/java/br/com/meli/cadastrofutebolapi/entities/services/Services.java
+++ cadastroFutebolAPI/src/main/java/br/com/meli/cadastrofutebolapi/entities/services/Services.java
@@ -1,4 +1,5 @@
 package br.com.meli.cadastrofutebolapi.entities.services;
 
 public class Services {
+
 }
